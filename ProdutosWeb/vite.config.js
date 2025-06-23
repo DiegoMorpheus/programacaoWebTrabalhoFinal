9cdaa18b785ab16dcbeffd0681880bd153c8e0d7
DiegoMorpheus <diego.morfeu@gmail.com>
2025-06-22 22:51:42 -0300

    TenCommit

--- ProdutosWeb/vite.config.js
+++ ProdutosWeb/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/ProdutosWeb/', // <- certinho se esse for o nome do repositório
   plugins: [react()],
 })
